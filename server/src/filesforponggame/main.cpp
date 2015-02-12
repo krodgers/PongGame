e45aad7743eba3e3ba436e412359ee013eea86dd
Kathryn Rodgers <rodgersk@uci.edu>
2015-02-11 23:36:17 -0800

    adding extra main

---
+++ server/src/filesforponggame/main.cpp
@@ -0,0 +1,48 @@
+#include "Server.h"
+#include "pong.h"
+#include <cstdlib>
+#ifdef __linux__
+#include <pthread.h>
+#elif _WIN32
+#include <process.h>
+#endif
+
+using namespace std;
+
+int serverThread;
+int gameLoopThread;
+//pong* pongGame;
+
+  extern pong* pongGame;
+
+int main(int argc, char *argv[]){
+	int port;
+	pongGame = new pong();
+
+	//setPongGame(*pongGame);
+
+	cout << "Please set server port: ";
+	cin >> port;
+
+#ifdef __linux__
+	pthread_t id;
+	void* voidStar = &port;
+	serverThread = pthread_create(&id, NULL, Server, voidStar);
+
+#elif _WIN32
+	_beginthread( Server, 0, (void*)port );
+#endif
+
+	
+
+
+	while (true)
+	{
+		if (pongGame->gameObjectSet == true)
+		{
+
+		}
+	}
+
+	return 1;
+}
