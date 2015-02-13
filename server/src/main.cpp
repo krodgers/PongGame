e435de58aad4d866e7278104517e7816ec03f988
Kathryn Rodgers <rodgersk@uci.edu>
2015-02-12 18:26:12 -0800

    added score updates to main.  Doesn't update unless the paddle is moved though....?

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -10,9 +10,9 @@
 #include <json/json.h>
 #include "pong.h"
 #include <cstdlib>
-#include <pthread.h>
 #ifdef __linux__
 #include <pthread.h>
+#include <unistd.h>
 #elif _WIN32
 #include <process.h>
 #endif
@@ -37,65 +37,84 @@ void* GameLoop(void*);
 
 
 int main(int argc, char *argv[]){
-	int port;
-	gameObjectsSet = false;
-	pongGame = new pong();
+  int port;
+  gameObjectsSet = false;
+  pongGame = new pong();
 
-	//setPongGame(*pongGame);
+  //setPongGame(*pongGame);
 
-	cout << "Please set server port: ";
-	cin >> port;
+  cout << "Please set server port: ";
+  cin >> port;
 
-	loopCount = 1;
+  loopCount = 1;
 
 #ifdef __linux__
-	pthread_t id;
-	void* voidStar = &port;
-	serverThread = pthread_create(&id, NULL, GameLoop, voidStar);
+  pthread_t id;
+  void* voidStar = &port;
+  serverThread = pthread_create(&id, NULL, GameLoop, voidStar);
 
 #elif _WIN32
-	_beginthread( Server, 0, (void*)port );
+  _beginthread( Server, 0, (void*)port );
 #endif
 
-    Server(port);
+  Server(port);
 
 
 }
 
 void* GameLoop(void* arg) {
-    while (true)
+  int scoreUpdateCounter = 0;
+  while (true)
+    {
+      if (gameObjectsSet == true)
 	{
-		if (gameObjectsSet == true)
-		{
-		    cout << "here" << endl;
-            pongGame->update();
+	  //  cout << "here" << endl;
+	  pongGame->update();
 
-            ostringstream json;
-            json << "{\"phase\":\"ball_update\",\"ball_position\":[";
-            json << pongGame->ballx << ", " << pongGame->bally << "]}";
-            vector<int> clientIDs = server.getClientIDs();
-            for (int i = 0; i < clientIDs.size(); i++){
-                server.wsSend(clientIDs[i], json.str());
-            }
-            usleep(1000000/60);
-		}
+	  ostringstream json;
+	  // {"phase":"ball_update", "ball_position":["x", "y"]}
+	  json << "{\"phase\":\"ball_update\",\"ball_position\":[";
+	  json << pongGame->ballx << ", " << pongGame->bally << "]}";
+	  
+	  vector<int> clientIDs = server.getClientIDs();
+	  
+	  // Only send score updates sometimes
+	  if(scoreUpdateCounter == 10){
+	    scoreUpdateCounter = 0;
+	    ostringstream jsonScoring;
+	    jsonScoring << "{\"phase\":\"score_update\",";
+	    jsonScoring << "\"new_score\":\"" << pongGame->score << "\", \"num_tries\":\"" << pongGame->totalTries << "\"}";
+	    for (int i = 0; i < clientIDs.size(); i++){
+	      server.wsSend(clientIDs[i], json.str());
+	      server.wsSend(clientIDs[i], jsonScoring.str());
+	    }
+	 
+	  }
+	  else{
+	    scoreUpdateCounter++;
+	    for (int i = 0; i < clientIDs.size(); i++){
+	      server.wsSend(clientIDs[i], json.str());
+	    }
+	  }
+	  usleep(1000000/60);
 	}
+    }
 }
 
 void Server(int port) {
-	server.setOpenHandler(openHandler);
-	server.setCloseHandler(closeHandler);
-	server.setMessageHandler(messageHandler);
-	//server.setPeriodicHandler(periodicHandler);
+  server.setOpenHandler(openHandler);
+  server.setCloseHandler(closeHandler);
+  server.setMessageHandler(messageHandler);
+  //server.setPeriodicHandler(periodicHandler);
 
-	/* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
-	server.startServer(port);
+  /* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
+  server.startServer(port);
 }
 
 void openHandler(int clientID){
 
-	string json = "{\"phase\":\"initialization\"}";
-	server.wsSend(clientID, json);
+  string json = "{\"phase\":\"initialization\"}";
+  server.wsSend(clientID, json);
 }
 
 /* called when a client disconnects */
@@ -104,123 +123,123 @@ void closeHandler(int clientID){
 }
 
 void checkPartnerPresent(int clientID) {
-	vector<int> clientIDs = server.getClientIDs();
-	int partner = -1;
-	for (int i = 0; i < clientIDs.size(); i++){
-		if (clientIDs[i] != clientID)
-			partner = clientIDs[i];
-	}
+  vector<int> clientIDs = server.getClientIDs();
+  int partner = -1;
+  for (int i = 0; i < clientIDs.size(); i++){
+    if (clientIDs[i] != clientID)
+      partner = clientIDs[i];
+  }
 
-	if (partner != -1) {
-		// There are two clients, so we should send
-		// both of them ball information and wait for
-		// both responses
-	} else {
-		// todo, here we would handle the case where
-		// one person is connected and not the other
-		// which should wait may or may not wait until
-		// the other player is connected
-		// Since we don't have another player right now
-		// it will just send data back to client
+  if (partner != -1) {
+    // There are two clients, so we should send
+    // both of them ball information and wait for
+    // both responses
+  } else {
+    // todo, here we would handle the case where
+    // one person is connected and not the other
+    // which should wait may or may not wait until
+    // the other player is connected
+    // Since we don't have another player right now
+    // it will just send data back to client
 
-		int ballRadius = pongGame->ballradius;
+    int ballRadius = pongGame->ballradius;
 
-		ostringstream json;
-		json << "{\"phase\":\"initial_ball_position\",\"ball_position\":[";
-		json << pongGame->ballx << ", " << pongGame->bally << "],";
-		json << "\"ball_size\":" << ballRadius << "}";
+    ostringstream json;
+    json << "{\"phase\":\"initial_ball_position\",\"ball_position\":[";
+    json << pongGame->ballx << ", " << pongGame->bally << "],";
+    json << "\"ball_size\":" << ballRadius << "}";
 
-		server.wsSend(clientID, json.str());
+    server.wsSend(clientID, json.str());
 
-	}
+  }
 }
 
 /* called when a client sends a message to the server */
 void messageHandler(int clientID, string message){
 
-	 // Let's parse it
-     Json::Value root;
-     Json::Reader reader;
-     bool parsedSuccess = reader.parse(message,
-                                       root,
-                                       false);
-    string phaseString = root["phase"].asString();
+  // Let's parse it
+  Json::Value root;
+  Json::Reader reader;
+  bool parsedSuccess = reader.parse(message,
+				    root,
+				    false);
+  string phaseString = root["phase"].asString();
 
-	if (phaseString.compare("initial_dimensions") == 0) {
-		const Json::Value mapDimJson = root["map_dimensions"];
-		const Json::Value paddleDimJson = root["paddle_dimensions"];
+  if (phaseString.compare("initial_dimensions") == 0) {
+    const Json::Value mapDimJson = root["map_dimensions"];
+    const Json::Value paddleDimJson = root["paddle_dimensions"];
 
-		vector<int> mapDims(mapDimJson.size());
-		vector<int> paddleDims(paddleDimJson.size());
+    vector<int> mapDims(mapDimJson.size());
+    vector<int> paddleDims(paddleDimJson.size());
 
-		cout << "mapJSOn size: " << mapDimJson.size() << "paddleJson Size: " << paddleDimJson.size() << endl;
+    cout << "mapJSOn size: " << mapDimJson.size() << "paddleJson Size: " << paddleDimJson.size() << endl;
 
-        cout << "Map Dimensions:" << endl;
-		for (int i = 0; i < mapDimJson.size(); i++) {
-			mapDims[i] = mapDimJson[i].asInt();
-			cout << mapDims[i] << endl;
-		}
+    cout << "Map Dimensions:" << endl;
+    for (int i = 0; i < mapDimJson.size(); i++) {
+      mapDims[i] = mapDimJson[i].asInt();
+      cout << mapDims[i] << endl;
+    }
 
-		cout << "Paddle Dimensions:" << endl;
-		for (int i = 0; i < paddleDimJson.size(); i++) {
-			paddleDims[i] = paddleDimJson[i].asInt();
-			cout << paddleDims[i] << endl;
-		}
+    cout << "Paddle Dimensions:" << endl;
+    for (int i = 0; i < paddleDimJson.size(); i++) {
+      paddleDims[i] = paddleDimJson[i].asInt();
+      cout << paddleDims[i] << endl;
+    }
 
-		pongGame->boardWidth = mapDims[2];
-		pongGame->boardHeight = mapDims[3];
-		pongGame->setPaddlePos(paddleDims[0], paddleDims[1]);
-		pongGame->setPaddleDimensions(paddleDims[2], paddleDims[3]);
-		pongGame->setBallPos(mapDims[2]/2, mapDims[3]/2);
-		pongGame->setBallRadius(10);
+    pongGame->boardWidth = mapDims[2];
+    pongGame->boardHeight = mapDims[3];
+    pongGame->setPaddlePos(paddleDims[0], paddleDims[1]);
+    pongGame->setPaddleDimensions(paddleDims[2], paddleDims[3]);
+    pongGame->setBallPos(mapDims[2]/2, mapDims[3]/2);
+    pongGame->setBallRadius(10);
 
-		//setMapInfo(mapDims, paddleDims);
-		checkPartnerPresent(clientID);
+    //setMapInfo(mapDims, paddleDims);
+    checkPartnerPresent(clientID);
 
-	} else if (phaseString.compare("ready_to_start") == 0) {
-		// todo, figure out what to do when we have two clients
-		// perhaps create a while loop that waits until another
-		// client has connected or something
+  } else if (phaseString.compare("ready_to_start") == 0) {
+    // todo, figure out what to do when we have two clients
+    // perhaps create a while loop that waits until another
+    // client has connected or something
 
-		// for now, just send start signal to single client
-		const Json::Value ballPosJson = root["ball_position"];
-		vector<int> ballPos(ballPosJson.size());
+    // for now, just send start signal to single client
+    const Json::Value ballPosJson = root["ball_position"];
+    vector<int> ballPos(ballPosJson.size());
 
-		for (int i = 0; i < ballPosJson.size(); i++) {
-			ballPos[i] = ballPosJson[i].asInt();
-		}
+    for (int i = 0; i < ballPosJson.size(); i++) {
+      ballPos[i] = ballPosJson[i].asInt();
+    }
 
-		if (ballPos[0] != pongGame->ballx || ballPos[1] != pongGame->bally)
-		{
-			// todo, do something to remedy situation where ball
-			// position is not correct
-		} else {
-			server.wsSend(clientID, "{\"phase\":\"start\"}");
-			gameObjectsSet = true;
-		}
-	} else if (phaseString.compare("paddle_update") == 0) {
-	    int paddleDirection = root["paddle_direction"].asInt();
-		const Json::Value paddlePosJson = root["paddle_position"];
-		vector<int> paddlePos(paddlePosJson.size());
-		for (int i = 0; i < paddlePosJson.size(); i++) {
-			paddlePos[i] = paddlePosJson[i].asInt();
-		}
-		/* todo, Now need to do stuff to actually use this information
-		** in the physics engine to update paddle location
-		*/
+    if (ballPos[0] != pongGame->ballx || ballPos[1] != pongGame->bally)
+      {
+	// todo, do something to remedy situation where ball
+	// position is not correct
+      } else {
+      server.wsSend(clientID, "{\"phase\":\"start\"}");
+      gameObjectsSet = true;
+    }
+  } else if (phaseString.compare("paddle_update") == 0) {
+    int paddleDirection = root["paddle_direction"].asInt();
+    const Json::Value paddlePosJson = root["paddle_position"];
+    vector<int> paddlePos(paddlePosJson.size());
+    for (int i = 0; i < paddlePosJson.size(); i++) {
+      paddlePos[i] = paddlePosJson[i].asInt();
+    }
+    /* todo, Now need to do stuff to actually use this information
+    ** in the physics engine to update paddle location
+    */
 
-        pongGame->setPaddleDirection(paddleDirection);
-		pongGame->setPaddlePos(paddlePos[0], paddlePos[1]);
-		/*cout << "Paddle x: " << paddlePos[0] << ", Paddle y: " << paddlePos[1] << endl;
-		int x = distribution(generator);
-		int y = distribution(generator);*/
+    pongGame->setPaddleDirection(paddleDirection);
+    pongGame->setPaddlePos(paddlePos[0], paddlePos[1]);
+    /*cout << "Paddle x: " << paddlePos[0] << ", Paddle y: " << paddlePos[1] << endl;
+      int x = distribution(generator);
+      int y = distribution(generator);*/
 
-		/*ostringstream json;
-		json << "{\"phase\":\"ball_update\",\"ball_position\":[";
-		json << x << ", " << y << "]}";
+    /*ostringstream json;
+      json << "{\"phase\":\"ball_update\",\"ball_position\":[";
+      json << x << ", " << y << "]}";
 
-		server.wsSend(clientID, json.str());*/
-	} else {
+      server.wsSend(clientID, json.str());*/
+  } else {
 
-	}
+  }
 }
